ed43b387f08dd034a4240127690510bbd32c7e50
barondugroove <chabot.benjamin@gmail.com>
2023-12-04 18:10:54 +0100

    dockercompose error corrected

--- frontend/angular/src/app/feature-modules/user/components/user-profile/user-profile.component.ts
+++ frontend/angular/src/app/feature-modules/user/components/user-profile/user-profile.component.ts
@@ -14,7 +14,6 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class UserProfileComponent {
 	gameList: Game[];
-	user: User[];
 
 	constructor(private offcanvas: NgbOffcanvas, private userService: UserService) {
 		this.gameList = [
@@ -25,8 +24,6 @@ export class UserProfileComponent {
 			{ id: 5, title: 'Basket', scoreOp1: 86, scoreOp2: 104, winner: 'John', loser: 'Jack' },
 			{ id: 6, title: 'Foot', scoreOp1: 2, scoreOp2: 1, winner: 'John', loser: 'Jack' },
 		];
-
-		this.user = this.getUserInfos();
 	}
 
 	editProfile() {
@@ -35,6 +32,6 @@ export class UserProfileComponent {
 	}
 
 	getUserInfos(): User {
-		return this.user;
+		return {email : 'lol', username: 'lol', password: 'lol'};
 	}
 }
