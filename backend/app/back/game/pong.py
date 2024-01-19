ce44bf9b5c32dcb1e0b340bc8234c5a848a775ca
Twintersh <tom.wintersheim@gmail.com>
2024-01-19 10:03:56 +0100

    game with canvas

--- backend/app/back/game/pong.py
+++ backend/app/back/game/pong.py
@@ -1,5 +1,6 @@
 import threading
 from turtle import update
+# import pygame
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from time import sleep, time
@@ -8,7 +9,7 @@ import random
 import json
 
 channel_layer = get_channel_layer()
-tick_rate = 1/30
+tick_rate = 1/50
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -27,16 +28,17 @@ ballMaxSpeed=7 # default value: 7
 
 pointsToWin=5 # default value: 5
 
+# clock = pygame.time.Clock()
+FPS=60
+
 class Ball:
-	def __init__(self, posX, posY, size, speed, acce):
+	def __init__(self, posX, posY, size, speed):
 		self.posx = posX
 		self.posy = posY
 		self.size = size
 		self.speedX = speed
 		self.speedY = speed/2
-		self.curSpeed = speed
 		self.speed = speed
-		self.acce = acce
 	
 	def move(self, paddles, score):
 		if (self.posx >= WIDTH or self.posx <= 0):
@@ -46,11 +48,6 @@ class Ball:
 				score[1] += 1 
 			self.posx = WIDTH / 2
 			self.posy = random.randint(HEIGHT * 0.25, HEIGHT * 0.75)
-			self.speedX = (abs(self.speedX)/self.speedX) * -self.speed # (abs(self.speedX/self.speedX)) to keep the current sign of speedX
-			self.speedY = ((abs(self.speedY)/self.speedY) * self.speed) / 2
-			self.curSpeed = self.speed
-			paddles[0].height = paddleHeight
-			paddles[1].height = paddleHeight
 		if (self.posy >= HEIGHT - ballSize or self.posy <= 0):
 			self.speedY *= -1
 		self.posx += self.speedX
@@ -62,13 +59,8 @@ class Ball:
 		if (angle == 0):
 			angle = 0.1
 
-		if (abs(self.curSpeed) < ballMaxSpeed):
-			self.curSpeed *= self.acce
-		elif (abs(self.speed) >= ballMaxSpeed):
-			self.curSpeed = (abs(self.curSpeed)/self.curSpeed) * -ballMaxSpeed
-
-		self.speedY = self.curSpeed * sin(radians(angle))
-		self.speedX = self.curSpeed
+		self.speedY = self.speed * sin(radians(angle))
+		self.speedX = self.speed
 
 		if (self.posx >= WIDTH/2):
 			self.speedX *= -1
@@ -100,11 +92,10 @@ class PongEngine(threading.Thread):
 		self.channel_layer = get_channel_layer()
 		self.inputLock = threading.Lock()
 		self.input = [0, 0]
-		# self.input = [0, 0]
 
 		self.paddle1 = Stricker(10, HEIGHT/2, paddleWidth, paddleHeight, paddleSpeed)
 		self.paddle2 = Stricker(WIDTH - paddleWidth - 10, HEIGHT/2, paddleWidth, paddleHeight, paddleSpeed)
-		self.ball = Ball(WIDTH/2, HEIGHT/2, ballSize, ballSpeed, ballAcceleration)
+		self.ball = Ball(WIDTH/2, HEIGHT/2, ballSize, ballSpeed)
 
 	def setWebsocket1(self, websocket):
 		self.websocket1 = websocket
@@ -121,8 +112,8 @@ class PongEngine(threading.Thread):
 
 	def collide(self, circle, paddle):
 		space = 0
-		if (circle.posx >= WIDTH/2):
-			space = ballSize
+		# if (circle.posx >= WIDTH/2):
+		# 	space = ballSize
 		if (circle.posx + space >= paddle.posx and circle.posx - space <= paddle.posx  + paddle.width):
 			if (circle.posy + space >= paddle.posy and circle.posy - space <= paddle.posy + paddle.height):
 				self.sendUpdates()
@@ -131,6 +122,7 @@ class PongEngine(threading.Thread):
 
 
 	def run(self):
+		self.start = time()
 		print("Started engine loop...")
 		score = [0, 0]
 		self.running = True
@@ -138,36 +130,35 @@ class PongEngine(threading.Thread):
 		move = [0, 0]
 	
 		while (self.running):
-			start = time()			
-			with self.inputLock:
-				inputs = self.input.copy()
+			if time() - self.start > 0.016:
+				with self.inputLock:
+					inputs = self.input.copy()
 
-			for i in range(2):
-				if (inputs[i] == 87):
-					move[i] = -1
-				elif (inputs[i] == 83):
-					move[i] = 1
-				else:
-					move[i] = 0
+				for i in range(2):
+					if (inputs[i] == 87):
+						move[i] = -1
+					elif (inputs[i] == 83):
+						move[i] = 1
+					else:
+						move[i] = 0
 
-			self.paddle1.posy += move[0] * self.paddle1.speed
-			self.paddle2.posy += move[1] * self.paddle2.speed
+				self.paddle1.posy += move[0] * self.paddle1.speed
+				self.paddle2.posy += move[1] * self.paddle2.speed
 
-			if self.collide(self.ball, self.paddle2):
-				self.ball.hit(self.paddle2)
-			if self.collide(self.ball, self.paddle1):
-				self.ball.hit(self.paddle1)
+				if self.collide(self.ball, self.paddle2):
+					self.ball.hit(self.paddle2)
+				if self.collide(self.ball, self.paddle1):
+					self.ball.hit(self.paddle1)
 
+				self.paddle1.check()
+				self.paddle2.check()
+				self.ball.move([self.paddle1, self.paddle2], score)
 
-			self.paddle1.check()
-			self.paddle2.check()
-			self.ball.move([self.paddle1, self.paddle2], score)
+				if (score[0] >= pointsToWin or score[1] >= pointsToWin):
+					self.running = False
 
-			if (score[0] >= pointsToWin or score[1] >= pointsToWin):
-				self.running = False
-
-			sleep(max(time() - start + (1/60 - time() - start), 0))
-			self.sendUpdates()
+				self.sendUpdates()
+				self.start = time()
 		return (score)
 
 	def sendUpdates(self):
