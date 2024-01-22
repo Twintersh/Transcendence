b9373833d20ad535222e15666051ac7b24a77b2c
Tom Wintersheim <91265339+Twintersh@users.noreply.github.com>
2024-01-22 17:36:06 +0100

    Game (#33)
    * push de l'amour avant vacances
    
    * push before pull
    
    * merge game
    
    * game with divs
    
    * game with canvas
    
    * Added endgame methods and updated Match model
    
    * Co-authored-by: Moïse <Moisewastaken@users.noreply.github.com>
    
    * game works
    
    ---------
    
    Co-authored-by: Moisewastaken <moise.cochin@gmail.com>

--- backend/app/back/back/urls.py
+++ backend/app/back/back/urls.py
@@ -20,10 +20,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users-swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('admin/', admin.site.urls),
+   path('users/', include('users.urls')),
+   path('users-swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	path('chat/', include('chat.urls')),
+	path('game/', include('game.urls'))
 ]
