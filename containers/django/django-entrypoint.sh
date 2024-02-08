8296d62499e8b5e4c5458b44c29c1b4256b61188
barondugroove <chabot.benjamin@gmail.com>
2024-02-08 15:21:45 +0100

    preparing merge inot main

--- containers/django/django-entrypoint.sh
+++ containers/django/django-entrypoint.sh
@@ -12,7 +12,7 @@ fi
 
 python manage.py flush --no-input
 python manage.py makemigrations
+python manage.py makemigrations users
 python manage.py migrate
 
 python manage.py runserver 0.0.0.0:8000
-exec "$@"
