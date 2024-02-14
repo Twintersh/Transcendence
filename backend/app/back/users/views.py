6c3f140b4767655406577aee3693fe9ffc66d25c
Moise Cochin <moise.cochin@gmail.com>
2024-02-14 16:49:22 +0100

    Updated last login and is active status

--- backend/app/back/users/views.py
+++ backend/app/back/users/views.py
@@ -27,6 +27,7 @@ def signup(request):
         avatar.save()
         user.set_password(request.data['password'])
         user.save()
+        update_last_login(User, request.user)
         token = Token.objects.create(user=user)
         return Response({'token': token.key}, status=status.HTTP_200_OK)
 
@@ -56,6 +57,7 @@ def isAuth(request):
 @permission_classes([IsAuthenticated])
 def logout(request):
     request.user.auth_token.delete()
+    update_last_login(User, request.user)
     return Response("User logged out", status=status.HTTP_200_OK)
 
 @swagger_auto_schema(method='POST', request_body=UserUpdateSerializer)
