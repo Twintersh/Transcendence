7f0281814a517adf96efe8dfafb52de07df6b077
Moise Cochin <moise.cochin@gmail.com>
2024-02-29 18:45:40 +0100

    Co-authored-by: tom.wintersheim@gmail.com <tom.wintersheim@gmail.com>

--- backend/app/back/back/routing.py
+++ backend/app/back/back/routing.py
@@ -1,14 +1,19 @@
-from .middlewares import TokenAuthMiddleWare
-from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "back.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
 import game.routing
-from django.core.asgi import get_asgi_application
+from .middlewares import TokenAuthMiddleWare
+
 
 application = ProtocolTypeRouter(
 	{
-		"http": get_asgi_application(),
+		"http": django_asgi_app,
 		"websocket": AllowedHostsOriginValidator(
 			TokenAuthMiddleWare(
 				URLRouter(
--- containers/django/django-entrypoint.sh
+++ containers/django/django-entrypoint.sh
@@ -15,4 +15,5 @@ python manage.py makemigrations
 python manage.py makemigrations users
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+daphne -e ssl:8000:privateKey=./ssl/nginx.key:certKey=./ssl/nginx.crt back.routing:application
