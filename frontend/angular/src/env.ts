90b27c7e3eaf497d3a61eb80aca8d0f1f466b7f6
Twintersh <tom.wintersheim@gmail.com>
2024-03-08 17:43:43 +0100

    Makefile change automatically the env.ts to set PROD_MODE to true or false

--- frontend/angular/src/env.ts
+++ frontend/angular/src/env.ts
@@ -1,3 +1,3 @@
-export const PROD_MODE = true
+export const PROD_MODE = false
 export const IP_SERVER = PROD_MODE ? '-ipserver-' : '127.0.0.1:8000'
 export const HTTP_MODE = PROD_MODE ? 'https://' : 'http://'
